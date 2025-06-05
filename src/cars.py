901482855dcfba44c0d446f0d74806e796b31f4d
Frank <frank.schulnies@gmail.com>
2025-06-05 10:53:25 +0200

    Update_App,Notebook cars_frank

--- src/cars.py
+++ src/cars.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from cars_functions import *
+import time
 
 df = read_df_cars()
 
@@ -8,21 +9,35 @@ st.header("Fahrzeugbestand")
 tab1, tab2 = st.tabs(["PKW Gesamt", "E-Car"])
 
 with tab1:
-    with st.container(border=True,height=520):
+    with st.container(border=True,height=550):
         change=(df["cars_sum"].iloc[0]-df["cars_sum"].iloc[-1])/df["cars_sum"].iloc[-1]*100
         st.markdown(f"##### :material/trending_up: {change:.1f}%")
         fig1=plot_cars(df)
         st.plotly_chart(fig1)
 
 with tab2:
-    with st.container(border=True,height=520):
+    with st.container(border=True,height=550):
         col1,col2=st.columns([1,3])
         with col1:
-            growth_factor = (df["e_cars"].iloc[0]/df["e_cars"].iloc[-1])
-            st.markdown(f"##### :material/arrow_upward: {growth_factor:.0f}-fache")
-
-
+            button=st.button("Start")
+            status_placeholder = st.empty()
         with col2:
             percent=st.checkbox("Anteil E-Car")
-        fig2=plot_e_cars(df,percent)
-        st.plotly_chart(fig2)
+
+        plot_placeholder = st.empty()
+
+        if button:
+            for i in range(1, len(df)+1):
+                df_filtered=df.iloc[-i::]
+                growth_factor = (df_filtered["e_cars"].iloc[0] / df_filtered["e_cars"].iloc[-1])
+                status_placeholder.markdown(f"##### :material/arrow_upward: {growth_factor:.0f}-fache")
+                fig3 = plot_e_cars(df_filtered)
+                plot_placeholder.plotly_chart(fig3)
+                time.sleep(0.1)
+        elif percent:
+            fig4=plot_e_cars_percent(df)
+            st.plotly_chart(fig4)
+        else:
+            fig2=plot_e_cars(df.loc[[df.index[-1], df.index[-2], df.index[0]]])
+            st.plotly_chart(fig2)
+
--- src/cars_functions.py
+++ src/cars_functions.py
@@ -27,14 +27,35 @@ def plot_cars(df):
     return fig
 
 @st.cache_data
-def plot_e_cars(df,percent):
-    if not percent:
-        fig = px.bar(df, x=df.index, y="e_cars", color_discrete_sequence=['red'])
-        fig.update_xaxes(title_text="")
-        fig.update_yaxes(title_text="Bestand E-Car")
-        return fig
-    else:
-        fig = px.line(df, x=df.index, y="%e_cars", color_discrete_sequence=['blue'], markers=True)
-        fig.update_xaxes(title_text="")
-        fig.update_yaxes(title_text="E-Car Anteil (%)")
-        return fig
+def plot_e_cars(df):
+    fig = px.bar(df, x=df.index, y="e_cars", color_discrete_sequence=['red'])
+    fig.update_xaxes(title_text="",range=['2018-06-01','2025-03-01'])
+    fig.update_yaxes(title_text="Bestand E-Car",range=[0, 1.8*1E6])
+
+    annotations = [
+        # Innovationsprämie (Juli 2020)
+        dict(
+            x='2020-07-01',  # Datum auf der X-Achse
+            y=0.3, yref='paper',  # y-Position (0=unten, 1=oben im Plotbereich)
+            text="Innovationsprämie startet<br>(Verdopplung Umweltbonus)",  # Text der Annotation
+            showarrow=True,  # Pfeil anzeigen
+            arrowhead=2,  # Art des Pfeilkopfs
+            ax=0, ay=-100,  # Pfeilversatz (ax=x-Versatz, ay=y-Versatz)
+            bgcolor="rgba(255, 255, 255, 0.8)",  # Hintergrundfarbe
+            bordercolor="gray",  # Randfarbe
+            borderwidth=1,  # Randbreite
+            borderpad=4,  # Abstand zum Rand
+            # font=dict(size=10, color="darkblue") # Schriftart
+        )]
+
+    fig.update_layout(annotations=annotations)
+
+    return fig
+
+
+@st.cache_data
+def plot_e_cars_percent(df):
+    fig = px.line(df, x=df.index, y="%e_cars", color_discrete_sequence=['blue'], markers=True)
+    fig.update_xaxes(title_text="")
+    fig.update_yaxes(title_text="E-Car Anteil (%)")
+    return fig
